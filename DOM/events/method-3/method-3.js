ccbdd0e8ed464a631aaefeca3ed6493f43e3fd44
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-11-14 17:59:43 -0500

    Practice with events

--- DOM/events/method-3/method-3.js
+++ DOM/events/method-3/method-3.js
@@ -1,11 +1,11 @@
-// the JavaScript file
-// METHODS 2 & 3
-function alertFunction() {
-  alert("YAY! YOU DID IT!");
-}
-const btn = document.querySelector("#btn");
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
 
-// METHOD 3
-btn.addEventListener("click", function (e) {
-  e.target.style.background = "blue";
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
 });
+
